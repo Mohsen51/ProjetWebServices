b97272f08cdb5af67f3ab300ab46254cd1c002c9
hvovar39 <66901674+hvovar39@users.noreply.github.com>
2020-10-05 20:57:56 +0200

    Client REST Java
    Client REST en Java fonctionnel

--- Client_REST_java/src/voyage/Client.java
+++ Client_REST_java/src/voyage/Client.java
@@ -4,43 +4,85 @@ import java.util.*;
 
 public class Client {
 
-	public static Voyageur inscription (Scanner sc) {
-		System.out.print ("INSCRIPTION :\nVeuillez entrez votre nom : ");
+	public static void inscription (Scanner sc, Requete rq) {
+		System.out.println ("INSCRIPTION :\nVeuillez entrez votre nom : ");
 		String nom = sc.next();
-		System.out.println("Veuillez entrez votre prenom : ");
+		System.out.println("INSCRIPTION : Veuillez entrez votre prenom : ");
 		String prenom = sc.next();
-		System.out.println("Veuillez entrez votre age : ");
-		int age = sc.nextInt();
-		System.out.println("Veuillez entrez votre adresse mail : ");
-		String mail = sc.next();
-		Voyageur v = new Voyageur (nom, prenom, age, mail);
-		return v;
+		System.out.println("INSCRIPTION : Veuillez entrez le nom du pays en minuscule: ");
+		String pays = sc.next();
+		rq.add_voyageur(nom, prenom, pays);
 	}
-	
-	public static void desinscription () {
-		System.out.println ("desinscription");
+
+	public static void desinscription (Scanner sc, Requete rq) {
+		System.out.println ("DESINSCRIPTION : Veuillez entrez l'ID du voyageur à désinscrire :");
+		int id = sc.nextInt();
+		rq.delete_voyageur(id);
 	}
-	
+
 	public static void informations (Scanner sc, Requete rq) {
-		System.out.println ("INFORMATIONS :\nSouhaitez vous consulter un pays en particulier ? (y/n)");
-		String s = sc.next();
-		if (s.equals("y")) {
-			System.out.println("Entrez le nom du pays en minuscule :");
-			s = sc.next();
-			rq.get_pays(s);
-		}else if (s.equals("n"))
-			rq.get_all_pays();
+		System.out.println("INFORMATIONS : Entrez 1 pour les informations sur les voyageurs.\nEntrez 2 pour les informations sur les pays.");
+		int i = sc.nextInt();
+		if (i==1) {
+			System.out.println ("INFORMATIONS : Souhaitez vous consulter un voyageur en particulier ? (y/n)");
+			String s = sc.next();
+			if (s.equals("y") ) {
+				System.out.println("INFORMATIONS : Entrez l'ID du voyageur :");
+				int j = sc.nextInt();
+				rq.get_voyageur(j);
+			} else {
+				rq.get_all_voyageur();
+			}
+		} else {
+			System.out.println ("INFORMATIONS : Souhaitez vous consulter un pays en particulier ? (y/n)");
+			String s = sc.next();
+			if (s.equals("y")) {
+				System.out.println("INFORMATIONS : Entrez le nom du pays en minuscule :");
+				s = sc.next();
+				rq.get_pays(s);
+			}else if (s.equals("n"))
+				rq.get_all_pays();
+		}
 	}
-	
-	public static void modification () {
-		System.out.println ("modification");
+
+	public static void modification (Scanner sc, Requete rq) {
+		System.out.println ("MODIFICATION : Veuillez entrez l'ID du voyageur :");
+		int id = sc.nextInt();
+		System.out.println("MODIFICATION : Veuillez entrez le nom du pays en minuscule :");
+		String pays = sc.next();
+		rq.update (id, pays);
 	}
-	
+
 	public static void main(String[] args) {
 		Requete rq = new Requete();
+		Scanner sc = new Scanner(System.in);
 		String s = "";
-		
-		rq.print_requete(s);
+		while (!s.equals("exit")) {
+			System.out.println("Que voulez-vous faire :\n"
+					+ "ins : inscription\n"
+					+ "des : desinscription\n"
+					+ "inf : information\n"
+					+ "mod : modification des regles d'un pays\n");
+			s = sc.next();
+			if (s.equals("ins")) {
+				inscription (sc, rq);
+			} else if (s.equals("des")) {
+				desinscription (sc, rq);
+			} else if (s.equals("inf")) {
+				informations (sc, rq);
+			} else if (s.equals("mod")) {
+				modification (sc, rq);
+			}
+		}
+
+//		Voyageur antoine = new Voyageur ("Antoine", "Dutest", 21, "antoine.dutest@efrei.net");
+//		Voyageur simon = new Voyageur ("Simon", "Tancev", 20, "simon.tancev@efrei.net");
+//		ArrayList<Voyageur> list_v = new ArrayList<Voyageur>();
+//		list_v.add(antoine);
+//		list_v.add(simon);
+//		Pays fr = new Pays ("France", 0, list_v);
+//		System.out.println(fr);
+		sc.close();	
 	}
 
 }
--- Client_REST_java/src/voyage/Requete.java
+++ Client_REST_java/src/voyage/Requete.java
@@ -11,7 +11,7 @@ public class Requete {
 	
 	public Requete () {
 		client = Client.create();
-		domaine = "https://api.covid19api.com/summary";
+		domaine = "http://localhost:8080/api/";
 	}
 	
 	public void get_pays(String s) {
@@ -22,12 +22,21 @@ public class Requete {
 	public void get_all_pays() {
 		print_requete ("pays");
 	}
+	
+	public void get_voyageur (int id) {
+		print_requete ("voyageurs/"+id);
+	}
 
+	public void get_all_voyageur() {
+		print_requete ("voyageurs");
+	}
+	
 	private String get_format (String s) {
 		s = s.toUpperCase();
 		char c = s.charAt(0);
 		s = s.toLowerCase();
-		s = s.replace(s.charAt(0), c);		
+		s = s.substring(1);
+		s = c+s;
 		return s;
 	}
 	
@@ -47,4 +56,67 @@ public class Requete {
 		System.out.println(output +"\n");
 	}
 	
+	public void add_voyageur (String nom, String prenom, String pays) {
+		String json = "{\"nomVoyageur\" : \""+ nom +"\" , \"prenomVoyageur\" : \""+ prenom +"\"}";
+		String path = get_format (pays);
+		post_requete("voyageur/" + path, json);
+		
+	}
+	
+	private void post_requete(String path, String json) {		 
+        WebResource webResource = client.resource(domaine + path);
+         
+        ClientResponse response = webResource.type("application/json").post(ClientResponse.class, json);
+ 
+        if (response.getStatus() != 200) {
+            System.out.println("Failed : HTTP error code : " + response.getStatus());
+            
+            String error= response.getEntity(String.class);
+            System.out.println("Error: "+error);
+            return;
+        }
+ 
+        System.out.println("Output from Server .... \n");
+        
+        String output = response.getEntity(String.class);
+        
+        System.out.println(output);
+    }
+	
+	public void update (int id, String pays) {
+		WebResource webResource = client.resource(domaine + "voyageurs/"+id);
+        String json = "{\"nomPays\" : \""+ get_format(pays) +"\"}";
+        
+        ClientResponse response = webResource.type("application/json").put(ClientResponse.class, json);
+ 
+        if (response.getStatus() != 200) {
+            System.out.println("Failed : HTTP error code : " + response.getStatus());
+            
+            String error= response.getEntity(String.class);
+            System.out.println("Error: "+error);
+            return;
+        }
+ 
+        System.out.println("Output from Server .... \n");
+        
+        String output = response.getEntity(String.class);
+        
+        System.out.println(output);
+	}
+	
+	public void delete_voyageur (int id) {
+		WebResource webResource = client.resource(domaine +"voyageurs/"+id);
+        
+        ClientResponse response = webResource.type("application/json").delete(ClientResponse.class);
+ 
+        if (response.getStatus() != 200) {
+            System.out.println("Failed : HTTP error code : " + response.getStatus());
+            
+            String error= response.getEntity(String.class);
+            System.out.println("Error: "+error);
+            return;
+        }
+ 
+        System.out.println("Voyageur désinscrit.\n");
+	}
 }
